616c80d7b4c112200d633aae7cecb66cf07f2240
Josh <joshhuang9508@gmail.com>
2025-04-19 23:59:02 +0800

    feat: 新增 transform-pwd.js 檔案，實作將帳戶密碼從 base64 解碼並使用 bcrypt 進行哈希處理，並更新至資料庫

--- transform-pwd.js
+++ transform-pwd.js
@@ -8,7 +8,14 @@ const main = async () => {
     const decodedStr = Buffer.from(account.password, 'base64').toString(
       'utf-8',
     );
+
     var hashed_password = await bcrypt.hash(decodedStr, 10);
+
+    console.log('帳號: ', account.account);
+    console.log('原始密碼: ', account.password);
+    console.log('解碼後: ', decodedStr);
+    console.log('雜湊後: ', hashed_password);
+    console.log('--------------------------------');
 
     await query(`UPDATE accounts SET password = ? WHERE account = ?`, [
       hashed_password,
