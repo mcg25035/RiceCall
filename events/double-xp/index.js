6d3fdd4d22004bf487358acee17ee9950126abc7
Josh <joshhuang9508@gmail.com>
2025-04-24 01:33:30 +0800

    feat: 更新活動配置，新增開始與結束時間，並根據時間動態設定活動啟用狀態

--- events/double-xp/index.js
+++ events/double-xp/index.js
@@ -3,8 +3,7 @@ const path = require('path');
 const config = require('./config.json');
 
 module.exports = {
-  enabled: config.enabled,
-  register: (client) => {
-    client.on('', () => {});
-  },
+  enabled:
+    Date.now() >= new Date(config.start) && Date.now() <= new Date(config.end),
+  register: (client) => {},
 };
