ac99663714c5c0ba40ff49e529aab75f2dbd4a95
Miyashooooo <cstrike.asia@gmail.com>
2025-04-25 21:18:09 +0800

    fix: user setting

--- src/components/UserInfoCard.tsx
+++ src/components/UserInfoCard.tsx
@@ -165,7 +165,9 @@ const UserInfoCard: React.FC<UserInfoCardProps> = React.memo(
 
               {/* Contribution */}
               <div className={userInfoCard['contributionBox']}>
-                <div className={userInfoCard['contributionIcon']}></div>
+                <div className={userInfoCard['contributionText']}>
+                  {lang.tr.contribution}:
+                </div>
                 <div className={userInfoCard['contributionValue']}>
                   {memberContributions}
                 </div>
